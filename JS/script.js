5a6776c2625b84e2f92b4ec64ea6c1ea03873970
Anunay Argha <anunayargha@gmail.com>
2024-02-18 18:39:59 +0600

    javaSript start

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,19 @@
 "use strict";
+
+const seatContainer = document.querySelector("#seats");
+const seatRemain = document.querySelectorAll("#seats p");
+const seatBooked = [];
+let totalSeat = 40;
+
+// * FUNCTION
+
+seatContainer.addEventListener("click", function (e) {
+	const clicked = e.target;
+	const greenColor = "!bg-primary";
+  const seatDetail = `
+  
+  
+
+  `
+	clicked.classList.add(greenColor);
+});
