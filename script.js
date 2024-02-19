183ea73496a9950e8e505c09cc9e14a883b15bf9
Anunay Argha <anunayargha@gmail.com>
2024-02-19 15:56:13 +0600

    js events

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ const grandPriceEl = document.getElementById("grand-price");
 const seatCounting = document.getElementById("seat-count");
 const totalSeats = document.getElementById("total-seats");
 const seatContainer = document.getElementById("seats");
+const discountContainer = document.getElementById("discount-container");
+const discountInput = document.getElementById("discount-input");
+const discountPriceContent = document.getElementById("discount-price");
 const discountCoupons = [...document.querySelectorAll(".coupon")].map(
 	(elment) => elment.textContent
 );
@@ -23,24 +26,30 @@ const phoneNumberInput = document.getElementById("phone");
 const discountBtn = document.getElementById("discount-btn");
 const nextBtn = document.getElementById("next-btn");
 
-let totalSeat = 40;
-let seatCount = 0;
+let [totalSeat, seatCount] = [40, 0];
 let totalPrice, discountPrice, grandPrice;
 
 // * FUNCTIONS -
 
 const calcPrice = function (seats, coupon) {
 	totalPrice = (seats * 550).toFixed(2);
-	if (coupon && discountCoupons.includes(coupon)) {
-		discountPrice = coupon === discountCoupons[0] ? 0.15 : 0.2 * totalPrice;
-	} else {
-		discountPrice = 0;
-	}
+	discountPrice = coupon
+		? (coupon === discountCoupons[0] ? 0.15 : 0.2) * totalPrice
+		: 0;
 	grandPrice = (totalPrice - discountPrice).toFixed(2);
 	totalPriceEl.textContent = totalPrice;
 	grandPriceEl.textContent = grandPrice;
 	totalSeats.textContent = totalSeat;
 	seatCounting.textContent = seats;
+	discountPriceContent.textContent = discountPrice.toFixed(2);
+};
+
+const btnActivate = function (value1, value2) {
+	if (value1 && value2 && seatCount > 0) {
+		nextBtn.removeAttribute("disabled");
+	} else {
+		nextBtn.setAttribute("disabled", true);
+	}
 };
 
 // * EVENT HANDLERS -
@@ -49,7 +58,7 @@ seatContainer.addEventListener("click", function (e) {
 	const clicked = e.target;
 	const greenColor = "!bg-primary";
 	const seatHtml = `
-	<div>
+	<div class='my-3'>
 		<p>${clicked.textContent}</p>
 		<p>Economoy</p>
 		<p>550</p>
@@ -66,13 +75,26 @@ seatContainer.addEventListener("click", function (e) {
 		seatCount++;
 		totalSeat--;
 		calcPrice(seatCount);
+		totalPriceEl.closest("div").classList.add("border-t-2");
 	}
 });
 
 // * BUTTONS EVENTS -
 
-discountBtn.addEventListener("click", function (e) {
-	console.log(e);
+couponInput.addEventListener("input", function () {
+	if (discountCoupons.includes(this.value) && seatCount > 0) {
+		discountBtn.removeAttribute("disabled");
+	} else {
+		discountBtn.setAttribute("disabled", true);
+	}
 });
 
-nextBtn.addEventListener("click", function () {});
+discountBtn.addEventListener("click", function () {
+	discountContainer.classList.remove("!hidden");
+	discountInput.classList.add("!hidden");
+	calcPrice(seatCount, couponInput.value);
+});
+
+nextBtn.addEventListener("click", function (e) {
+	e.preventDefault();
+});
