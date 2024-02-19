5d0cb51f50a2e7ddbc877fe4ef3f00f914b6233c
Anunay Argha <anunayargha@gmail.com>
2024-02-19 10:31:46 +0600

    project continue

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,41 @@
 "use strict";
 
+// * SELECTED ELEMENTS -
+
 const seatContainer = document.querySelector("#seats");
-const seatRemain = document.querySelectorAll("#seats p");
-const seatBooked = [];
+const bookContainer = document.getElementById("booking");
+const totalPrice = document.getElementById("total-price");
+const grandPrice = document.getElementById("grand-price");
+const discountBtn = document.getElementById("discount-btn");
+const discountCoupons = [...document.querySelectorAll(".coupon")].map(
+	(elment) => elment.textContent
+);
+
 let totalSeat = 40;
+let seatCount = 0;
 
 // * FUNCTION
+
+// * EVENT HANDLERS -
 
 seatContainer.addEventListener("click", function (e) {
 	const clicked = e.target;
 	const greenColor = "!bg-primary";
-  const seatDetail = `
-  
-  
+	const seatHtml = `
+	<div>
+		<p>${clicked.textContent}</p>
+		<p>Economoy</p>
+		<p>550</p>
+	</div>
+	`;
 
-  `
-	clicked.classList.add(greenColor);
+
+
+	if (!clicked.classList.contains(greenColor) && seatCount < 4) {
+		clicked.classList.add(greenColor);
+		bookContainer.insertAdjacentHTML("afterbegin", seatHtml);
+		seatCount++;
+		totalPrice.textContent = seatCount * 550;
+		grandPrice.textContent = totalPrice.textContent;
+	}
 });
